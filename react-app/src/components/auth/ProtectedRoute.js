875a22596c58d06ceec2717e87fd8274d1134051
mitchellreiss <mitchellreiss@gmail.com>
2021-04-01 10:21:49 -0700

    Fix: 🚑  Protected Route import path

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { useCurrentUser } from '../../hooks/user'
 
 const ProtectedRoute = (props) => {
-  if (!props.authenticated) {
+  const authenticated = !!useCurrentUser()
+  if (!authenticated) {
     return <Redirect to="/login" />
   }
   return <Route {...props} />

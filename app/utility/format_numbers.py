82367c134d64759f1d34d04716d5727cceac78ac
mhrgoldberg <mitchellreiss@gmail.com>
2022-02-16 15:16:11 -0800

    Refactor: Update number formatting

--- app/utility/format_numbers.py
+++ app/utility/format_numbers.py
@@ -9,7 +9,7 @@ def get_feet(i: float) -> str:
     """
     Convert meters to feet and format to 2 decimal places with commas between thousands
     """
-    return format(i * 3.28084, ",.2f")
+    return format(i * 3.28084, ",.1f")
 
 
 def format_decimals(i: float) -> str:
